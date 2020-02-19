71acc4306a2e5e3129fd29fd0fce357ee48ae663
HungNhOnPV <anhhungx9propro@gmail.com>
2020-02-19 16:37:44 +0700

    bulding modal add work

--- src/components/TaskForm/index.js
+++ src/components/TaskForm/index.js
@@ -1,49 +1,62 @@
-import { TextField, withStyles } from '@material-ui/core';
+import { TextField, withStyles, Modal, Box } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import React, { Component } from 'react';
 import styles from './styles';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
 
 class TaskForm extends Component {
   render() {
-    const { open, classes, handleClose } = this.props;
+    const { classes, open, handleClose } = this.props;
     return (
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Add new work</DialogTitle>
-        <DialogContent>
-          <TextField
-            id="standard-name"
-            label="Name"
-            className={classes.textField}
-            margin="normal"
-          />
-          <TextField
-            id="standard-multiline-flexible"
-            label="Multiline"
-            multiline
-            rowsMax="4"
-            className={classes.textField}
-            margin="normal"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            OK
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <Modal open={open} onClose={handleClose}>
+        <div className={classes.modal}>
+          <form>
+            <Grid container>
+              <Grid item md={12}>
+                <TextField
+                  id="standard-name"
+                  label="Title"
+                  className={classes.textField}
+                  margin="normal"
+                />
+              </Grid>
+              <Grid item md={12}>
+                <TextField
+                  id="standard-name"
+                  label="Describe"
+                  className={classes.textField}
+                  margin="normal"
+                />
+              </Grid>
+              <Grid item md={12}>
+                <Box display="flex" flexDirection="row-reverse" mt={2}>
+                  <Box ml={1}>
+                    <Button
+                      variant="contained"
+                      color="default"
+                      onClick={handleClose}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
+                  <Button variant="contained" color="primary">
+                    Save
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Modal>
     );
   }
+}
+
+TaskForm.propTypes = {
+  classes: PropTypes.object,
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
 }
 
 export default withStyles(styles)(TaskForm);
